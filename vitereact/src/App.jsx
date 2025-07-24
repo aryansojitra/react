45debe1be6ee25c0054b772fb1710f055c166681
aryan sojitra <aryanpatel0344h@gmail.com>
2025-07-24 23:36:46 +0530

    Refactor App component to include username and additional content; add custom rendering logic in customReact.js and update index.html

--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import Chai from './chai'
 
+/*function MyApp(){
+  return(
+    <div>
+      <h1>custom app | chai</h1>
+    </div>
+  )
+}*/
 
 function App() {
+const username = "aryan sojitra"
+//{username} evaluted expression
 
   return (
-  <Chai/>
+  <>
+    <Chai/>
+    <h1>chai aur code {username}</h1> 
+    <p>test para</p>
+  </>
   )
 }
 
--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -1,9 +1,21 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const anotherElement = (
+  <a href="https://google.com" target='-bland'>visit google</a>
+)
+
+const anotherUser = "aryan rank"
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com',target: '_blank'},
+  'click me to visit google',
+  anotherUser
+
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ reactElement
 )
