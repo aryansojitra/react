962aa502de81a9eb0fd1251e0ec127846c7617cf
aryan sojitra <aryanpatel0344h@gmail.com>
2025-08-04 19:24:48 +0530

    feat: initialize React project with Vite, add user context for authentication
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG and React logo SVG assets.
    - Implemented basic styling in App.css and index.css.
    - Developed main application structure in App.jsx, including UserContextProvider.
    - Created Login and Profile components for user authentication.
    - Established UserContext and UserContextProvider for managing user state.
    - Configured Vite for building and serving the application.

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider.jsx'
+import Login from './components/Login.jsx'
+import Profile from './components/Profile.jsx'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1>react with aryan</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
